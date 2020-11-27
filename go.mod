865e638beb2fc4a8dfc719bad1e6e25f35b128f9
Christoph Schulz <christoph.2.schulz@atos.net>
2020-11-27 23:02:52 +0100

    Example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/atosorigin/kustomize-value-transfomer
 
 go 1.15
+
+require sigs.k8s.io/kustomize/kyaml v0.10.1
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"sigs.k8s.io/kustomize/kyaml/kio"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+func main() {
+	input := bytes.NewReader([]byte(`apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+  labels:
+    app: nginx
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.7.9
+        ports:
+        - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx
+spec:
+  selector:
+    app: nginx
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 80
+`))
+
+	// setAnnotationFn
+	setAnnotationFn := kio.FilterFunc(func(operand []*yaml.RNode) ([]*yaml.RNode, error) {
+		for i := range operand {
+			resource := operand[i]
+			_, err := resource.Pipe(yaml.SetAnnotation("foo", "bar"))
+			if err != nil {
+				return nil, err
+			}
+		}
+		return operand, nil
+	})
+
+	err := kio.Pipeline{
+		Inputs:  []kio.Reader{&kio.ByteReader{Reader: input}},
+		Filters: []kio.Filter{setAnnotationFn},
+		Outputs: []kio.Writer{kio.ByteWriter{Writer: os.Stdout}},
+	}.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
