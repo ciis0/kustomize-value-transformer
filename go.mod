0c55a34eda8448a8c0fe9fdf269cf1aca5a48385
Christoph Schulz <christoph.2.schulz@atos.net>
2020-11-28 01:30:10 +0100

    First working version

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/atosorigin/kustomize-value-transfomer
 
 go 1.15
 
-require sigs.k8s.io/kustomize/kyaml v0.10.1
+require (
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.10.1
+)
--- main.go
+++ main.go
@@ -2,53 +2,25 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/Masterminds/sprig"
 	"log"
 	"os"
 	"sigs.k8s.io/kustomize/kyaml/kio"
+	"sigs.k8s.io/kustomize/kyaml/openapi"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
+	"sigs.k8s.io/kustomize/kyaml/yaml/walk"
+	"text/template"
 )
 
+// created based kio example
+// https://pkg.go.dev/sigs.k8s.io/kustomize/kyaml@v0.10.1/kio#example-package
 func main() {
-	input := bytes.NewReader([]byte(`apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: nginx
-  labels:
-    app: nginx
-spec:
-  replicas: 3
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      containers:
-      - name: nginx
-        image: nginx:1.7.9
-        ports:
-        - containerPort: 80
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx
-spec:
-  selector:
-    app: nginx
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 80
-`))
 
-	// setAnnotationFn
-	setAnnotationFn := kio.FilterFunc(func(operand []*yaml.RNode) ([]*yaml.RNode, error) {
+	fn := kio.FilterFunc(func(operand []*yaml.RNode) ([]*yaml.RNode, error) {
 		for i := range operand {
 			resource := operand[i]
-			_, err := resource.Pipe(yaml.SetAnnotation("foo", "bar"))
+			_, err := Walker(resource)
 			if err != nil {
 				return nil, err
 			}
@@ -57,12 +29,48 @@ spec:
 	})
 
 	err := kio.Pipeline{
-		Inputs:  []kio.Reader{&kio.ByteReader{Reader: input}},
-		Filters: []kio.Filter{setAnnotationFn},
+		Inputs:  []kio.Reader{&kio.ByteReader{Reader: os.Stdin}},
+		Filters: []kio.Filter{fn},
 		Outputs: []kio.Writer{kio.ByteWriter{Writer: os.Stdout}},
 	}.Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+type Visitor struct{}
+
+func Walker(rn *yaml.RNode) (*yaml.RNode, error) {
+	return walk.Walker{
+		Visitor: Visitor{},
+		Sources: []*yaml.RNode{rn}}.Walk()
+}
+
+func (m Visitor) VisitScalar(nodes walk.Sources, _ *openapi.ResourceSchema) (*yaml.RNode, error) {
+
+	str := nodes.Dest().YNode().Value
+	buf := new(bytes.Buffer)
+
+	tmpl, err := template.New("test").Option("missingkey=error").Delims("§((", "))").Funcs(sprig.TxtFuncMap()).Parse(str)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = tmpl.Execute(buf, map[string]string{"foo": "hello\nthere\nstranger"})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return yaml.NewScalarRNode(buf.String()), nil
+}
+
+func (m Visitor) VisitMap(nodes walk.Sources, _ *openapi.ResourceSchema) (*yaml.RNode, error) {
+	return nodes.Dest(), nil
+}
+
+func (m Visitor) VisitList(nodes walk.Sources, _ *openapi.ResourceSchema, _ walk.ListKind) (*yaml.RNode, error) {
+	return nodes.Dest(), nil
 }
